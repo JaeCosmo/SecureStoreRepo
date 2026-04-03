f65f28363d74c7996a43681b4a9bb199f43da072
JaeCosmo <joshua.clarke@georgebrown.ca>
2026-04-03 17:31:23 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { Amplify } from 'aws-amplify';
+
+Amplify.configure({
+  Auth: {
+    region: 'us-east-1',
+    userPoolId: 'us-east-1_dB7LLqLBD',
+    userPoolWebClientId: '63iu7b142rnpi513342cpmoq1d',
+  },
+  API: {
+    GraphQL: {
+      endpoint: 'https://n7sry4lnivbf3kh5wu37a27nbi.appsync-api.us-east-1.amazonaws.com/graphql',
+      region: 'us-east-1',
+      defaultAuthMode: 'userPool'
+    }
+  }
+});
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
